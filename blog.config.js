36d33dbc3a8d36bd179fb73c8ce2bdc158ca36c4
workingSmallQ <smallsmallqwq@gmail.com>
2024-10-15 23:30:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '崇实学辅', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '崇实书院学业辅导与发展中心', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://csxf.online', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://helping-center-git-main-workingsmallqs-projects.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 分享', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
