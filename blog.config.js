f6971cbba0d6630a4c60028e1fa482b60eee630b
workingSmallQ <smallsmallqwq@gmail.com>
2024-10-19 11:27:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -27,7 +27,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '崇实学辅', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '崇实书院学业辅导与发展中心', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.csxf.online/', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 分享', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '崇实学辅, 资料 , 西安交通大学 , 西安交大 , 崇实书院', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
